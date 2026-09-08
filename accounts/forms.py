44e2e49da103e5268dd125eb0d211baf7eab87f5
sadaukihop-png <sadaukihop@gmail.com>
2026-09-08 23:03:08 +0100

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,1 +1,60 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
 
+class AdminLoginForm(forms.Form):
+    """
+    Admin login form
+    """
+    username = forms.CharField(
+        max_length=150,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter admin username'
+        })
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter admin password'
+        })
+    )
+
+class GroupLeaderLoginForm(forms.Form):
+    """
+    Group Leader login form
+    """
+    group_name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter group name (e.g., Group A)'
+        })
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter group password'
+        })
+    )
+
+class AdminCreationForm(UserCreationForm):
+    """
+    Form to create the super admin (first-time setup)
+    """
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=100, required=True)
+    last_name = forms.CharField(max_length=100, required=True)
+    
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        if commit:
+            user.save()
+        return user
